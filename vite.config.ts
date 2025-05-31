1a2c0fbcf9df28e2d7239a145f53fa3f8b12a090
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2025-05-31 17:07:32 +0800

    feat: 在 package.json 和 pnpm-lock.yaml 中添加 vite-plugin-vue-devtools 依赖，并在 vite.config.ts 中配置插件以支持 Vue 开发工具

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': path.resolve('./src'),
