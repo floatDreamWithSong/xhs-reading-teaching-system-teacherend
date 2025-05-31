99012029f63aeb2a3cbc858fb13e9b9bf40bb2cb
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2025-05-31 15:53:24 +0800

    feat: 新增 BigButton 组件并在 BankPage 和 CreateClassPage 中使用，重构创建班级页面以使用新按钮组件

---
+++ src/components/common/BigButton.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+}
+interface Emits {
+  (e: 'click'): void
+}
+
+defineProps<Props>()
+
+const emit = defineEmits<Emits>()
+const handleClick = () => {
+  emit('click')
+}
+</script>
+<template>
+  <div class="button-container">
+    <button @click="handleClick" class="button">
+      {{ text }}
+    </button>
+  </div>
+</template>
+<style scoped>
+.button-container {
+  width: 100%;
+
+  .button {
+    display: block;
+    margin: auto;
+    box-shadow: 0px 2px 3.6px rgba(0, 0, 0, 0.05);
+    border-radius: 10px;
+    width: 240px;
+    height: 45px;
+    font-size: 16px;
+    letter-spacing: 0.3em;
+    text-align: center;
+  }
+}
+</style>
--- src/views/BankPage.vue
+++ src/views/BankPage.vue
@@ -1,10 +1,55 @@
 <script setup lang="ts">
+import BigButton from '@/components/common/BigButton.vue';
+import Select from '@/components/common/Select.vue';
+import TextInput from '@/components/common/TextInput.vue';
+import { Search } from '@/components/icons';
+
 </script>
 
 <template>
   <div class="content-header">
     题库管理
   </div>
+  <div class="card-area filter-options">
+    <Select label="题型:" placeholder="全部题型" />
+    <Select label="难度:" placeholder="全部难度" />
+    <TextInput label="搜索:" placeholder="题目关键词">
+      <template #suffix>
+        <Search />
+      </template>
+    </TextInput>
+  </div>
+  <div class="card-area filter-result">
+    <div class="card-header">
+      <div class="card-header-title">
+
+      </div>
+    </div>
+  </div>
+  <BigButton text="新建题目" />
 </template>
 <style scoped>
+.card-area {
+  padding: 18px 12px;
+
+  &.filter-options {
+    margin-top: 12px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 25px;
+  }
+
+  &.filter-result {
+    margin-bottom: 58px;
+  }
+}
+
+.create-question-button-container {
+  margin-top: 58px;
+
+  .create-question-button {
+    display: block;
+    margin: auto;
+  }
+}
 </style>
