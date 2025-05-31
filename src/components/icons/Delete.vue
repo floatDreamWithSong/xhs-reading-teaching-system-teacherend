b2c144cb0beab7c5203d150f94b8fca2c4891eea
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2025-05-31 20:32:01 +0800

    feat: 新增多个图标组件，包括 Delete、Edit 和 Student，并更新图标索引文件以包含新组件，同时添加 classroom.png 图片文件

---
+++ src/components/icons/Delete.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+<template>
+  <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M4.58545 5.68695H19.3175M9.18921 2.76221H14.7137M10.11 15.4361V9.5866M13.793 15.4361V9.5866M15.1741 19.3357H8.72883C7.7118 19.3357 6.88733 18.4628 6.88733 17.3859L6.46691 6.70245C6.44512 6.14857 6.86331 5.68695 7.38687 5.68695H16.5161C17.0396 5.68695 17.4578 6.14857 17.436 6.70245L17.0156 17.3859C17.0156 18.4628 16.1911 19.3357 15.1741 19.3357Z"
+      stroke="#A9A9A9" stroke-width="1.8415" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+
+</template>
+<style scoped></style>
